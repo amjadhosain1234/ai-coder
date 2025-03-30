1b41a89b3ac6db03b98d071ca418be5f7112d50a
Jamal Uddin <140267521+amjadhosain1234@users.noreply.github.com>
2025-03-30 23:23:13 +0600

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,6 +1,6 @@
 import OpenAI from "openai";
 
-const token = process.env["GITHUB_TOKEN"];
+const token = process.env["OPENAI_API_KEY"];
 const endpoint = "https://models.inference.ai.azure.com";
 const modelName = "gpt-4o";
 
